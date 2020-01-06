bd833029967e82a66949b8ffe888dcf95e60e25c
Nina Angelo <ninacelie@gmail.com>
2020-01-06 21:27:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,11 @@
 class Author
-  attr_accessor :name
+  attr_accessor :name, :posts
 
   def initialize(name)
     @name = name
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 end
