42837b163326653647a26e9a648e02a10f623364
Nina Angelo <ninacelie@gmail.com>
2020-01-06 20:00:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,17 @@
+
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end
 
 end
